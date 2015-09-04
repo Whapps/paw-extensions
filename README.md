30846bb412e22eafeddd7ea51813bb7072d4a6a7
Matt Zorn <zornme@gmail.com>
2015-09-04 16:57:00 -0400

    initial

---
+++ README.md
@@ -0,0 +1,12 @@
+# What is this?
+
+This is a directory for [Paw](https://luckymarmot.com/paw/) extensions. This repository is __public__ so make sure no secrets are committed. Make them configurable!
+
+# What do the extensions do?
+
+The first extension (com.online-rewards.PawExtensions.Rpc) signs Online-Rewards RPC requests. Once installed usage is simple:
+
+![com.online-rewards.PawExtensions.Rpc usage](com.online-rewards.PawExtensions.Rpc/usage.gif)
+
+# How do I install them?
+Copy the the individual extension directories to `~/Library/Containers/com.luckymarmot.Paw/Data/Library/Application Support/com.luckymarmot.Paw/Extensions/`.
---
+++ com.online-rewards.PawExtensions.Rpc/Rpc.js
@@ -0,0 +1,24 @@
+(function() {
+    require("./vendor/crypto-js/crypto-js.js");
+    var Rpc;
+
+    Rpc = function() {
+        this.evaluate = function(context) {
+            var request     = context.getCurrentRequest(),
+                url         = context.getCurrentRequest().getUrl(),
+                queryString = url.substring(url.indexOf("?") + 1),
+                body        = request.method === "GET" ? queryString : request.body;
+            return CryptoJS.HmacSHA256(body, this.secret);
+        };
+
+        this.title = function() {
+            return "Online-Rewards Rpc Signature";
+        };
+    };
+
+    Rpc.identifier = "com.online-rewards.PawExtensions.Rpc";
+    Rpc.title      = "Online-Rewards Rpc Signature";
+    Rpc.inputs     = [ DynamicValueInput("secret", "Secret", "String") ];
+
+    registerDynamicValueClass(Rpc);
+}).call(this);
